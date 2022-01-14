1511c360474794e87c819a87130a130bdc02ace6
mridul rahman <mridulrahman98@gmail.com>
2022-01-14 11:31:48 +0600

    manage

---
+++ lib/main.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/models/product_list.dart';
+import 'package:shop_app/widgets/home/sliver_adaptor.dart';
+import 'package:shop_app/widgets/home/sliver_data.dart';
+import 'package:shop_app/widgets/product_card.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: ProductProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          drawer: Drawer(  
+        child: ListView(  
+          // Important: Remove any padding from the ListView.  
+          padding: EdgeInsets.zero,  
+          children: <Widget>[  
+            UserAccountsDrawerHeader(  
+              accountName: Text("Mizanur Rahman Mridul"),  
+              accountEmail: Text("mridulrahman98@gmail.com"),  
+              currentAccountPicture: CircleAvatar(  
+                backgroundColor: Colors.orange,  
+                child: Text(  
+                  "A",  
+                  style: TextStyle(fontSize: 40.0),  
+                ),  
+              ),  
+            ),  
+            ListTile(  
+              leading: Icon(Icons.home), title: Text("Home"),  
+              onTap: () {  
+                Navigator.pop(context);  
+              },  
+            ),  
+            ListTile(  
+              leading: Icon(Icons.settings), title: Text("Settings"),  
+              onTap: () {  
+                Navigator.pop(context);  
+              },  
+            ),  
+            ListTile(  
+              leading: Icon(Icons.contacts), title: Text("Contact Us"),  
+              onTap: () {  
+                Navigator.pop(context);  
+              },  
+            ),  
+          ],  
+        ),  
+      ),  
+          body: CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.work_outline),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.person),
+                  ),
+                ],
+                backgroundColor: Color(0xFF042925),
+                title: Text('cosmetics'),
+                centerTitle: true,
+                pinned: true,
+                expandedHeight: 120,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.only(
+                        top: 80.0, bottom: 20.0, left: 10.0, right: 10.0),
+                    child: TextField(
+                      style: TextStyle(color: Colors.white, letterSpacing: 2.0),
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          contentPadding: EdgeInsets.only(left: 10.0),
+                          filled: true,
+                          focusColor: Colors.white,
+                          focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.white, width: 2),
+                              borderRadius: BorderRadius.circular(26.0)),
+                          enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color(0xFFf9f7f7), width: 1.0),
+                              borderRadius: BorderRadius.circular(26.0))),
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 10.0),
+                        child: Row(
+                          children: [
+                            Text(
+                              'Home ',
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  letterSpacing: 1,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 10,
+                              child: Text('/',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      letterSpacing: 1,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                            Text(' Cosmetics ',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    letterSpacing: 1,
+                                    fontWeight: FontWeight.bold)),
+                           /* SizedBox(
+                              width: 10,
+                              child: Text('/',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      letterSpacing: 1,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                            Text(' Ac',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    letterSpacing: 1,
+                                    fontWeight: FontWeight.bold)),
+                                    */
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              HomeSliverAdapter(),
+              SliverProduct()
+            ],
+          ),
+          endDrawer: Drawer(
+            child: ListView(
+              children: [],
+            ),
+          ),
+          //body: ProductCard(),
+        ),
+        // routes: {
+        //   '/': (context) => ProductCard(),
+        // },
+      ),
+    );
+  }
+}
---
+++ lib/models/product_list.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shop_app/models/product.dart';
+
+class ProductProvider with ChangeNotifier {
+  final List<Product> _productList = [
+    Product(
+        productId: 'c022',
+        name: 'Makeup Kit',
+        price: 400.0,
+        discount: 20.0 ,
+        categoryName: 'cosmetics',
+        subCategoryName: 'phones and tabs',
+        brandName: 'Brand: Mac',
+        warrenty: '1 years',
+        imageUrl:
+            'https://media.istockphoto.com/photos/makeup-palette-and-brushes-professional-eyeshadow-palette-picture-id1299164489?b=1&k=20&m=1299164489&s=170667a&w=0&h=GqoYQHfqq4UfbHFq9TlUp3isRCAqm9hoFqnxaKUEQEU=',
+        productDetails: 'Best Tablet'),
+    Product(
+        productId: 'c023',
+        name: 'Lipstick',
+        price: 80.0,
+        discount: 0.0,
+        categoryName: 'cosmetics',
+        subCategoryName: 'phones and tabs',
+        brandName: 'Brand: MRM',
+        warrenty: '2 Month',
+        imageUrl:
+            'https://www.greenqueen.com.hk/wp-content/uploads/2021/02/Tinge-Young-Startup-Disrupts-Indian-Beauty-Market-With-Customisable-Vegan-Eco-Cosmetics.jpg',
+        productDetails: 'Best Tablet'),
+    Product(
+        productId: 'c024',
+        name: 'Healthy Skin Nourishing Lip Balm',
+        price: 300.0,
+        discount: 0.0,
+        categoryName: 'cosmetics',
+        subCategoryName: 'phones and tabs',
+        brandName: 'Brand: MRM',
+        //warrenty: '2 years',
+        imageUrl:
+            'https://static.standard.co.uk/2021/07/20/14/111skinmaskjpg?width=640&auto=webp&quality=50&crop=1500%3A1000%2Csmart',
+        productDetails: 'Best Tablet'),
+    Product(
+        productId: 'c025',
+        name: 'Indian Beauty ',
+        price: 80.0,
+        discount: 20.0,
+        categoryName: 'cosmetics',
+        subCategoryName: 'phones and tabs',
+        brandName: 'Mizan Group',
+        //warrenty: '2 years',
+        imageUrl:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5airOlVleSzg58vNYuzNv7F6hSgSojqpJOA&usqp=CAU',
+        productDetails: 'Best Phone'),
+    Product(
+        productId: 'c026',
+        name: 'GIrls Sungluss',
+        price: 150.0,
+        discount: 20.0,
+        categoryName: 'cosmetics',
+        subCategoryName: 'smart device',
+        brandName: 'Mizan Group',
+        //warrenty: '2 years',
+        imageUrl:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOScgJir5rZfc8vrb3EsYMwcb5I8Uq8iKZYg&usqp=CAU',
+        productDetails: 'Smart Audio Device'),
+    Product(
+        productId: 'c027',
+        name: 'Girls Razor',
+        price: 40.0,
+        discount: 10.0,
+        categoryName: 'cosmetics',
+        subCategoryName: 'PC and Mac',
+        brandName: 'GOL',
+        //warrenty: '2 years',
+        imageUrl:
+            'https://image.shutterstock.com/image-photo/pink-lady-shaver-on-white-260nw-65601124.jpg',
+        productDetails: 'Best Laptop'),
+    Product(
+        productId: 'c028',
+        name: 'Eyeliner',
+        price: 145.0,
+        discount: 20.0,
+        categoryName: 'cosmetics',
+        subCategoryName: 'PC and Mac',
+        brandName: '',
+        //warrenty: '2 years',
+        imageUrl:
+            'https://m.media-amazon.com/images/I/61r+yeaIgAL._AC_SL1500_.jpg',
+        productDetails: 'Best All in on PC'),
+    Product(
+        productId: 'c029',
+        name: 'Metal Earrings for Women',
+        price: 660.0,
+        discount: 10.0,
+        categoryName: 'cosmetics',
+        subCategoryName: 'PC and Mac',
+        brandName: 'CTG',
+        warrenty: '1 years',
+        imageUrl:
+            'https://static-01.daraz.com.bd/p/702c5d31c942c768582e3922814b0b73.jpg',
+        productDetails: 'Best Laptop'),
+    
+  ];
+  int _crossAxisCount = 2;
+  double _expandedHeight = 280;
+  double _imageHeight = 160;
+  int get crossAxisCounts => _crossAxisCount;
+  double get expandedHeight => _expandedHeight;
+  double get imageHeight => _imageHeight;
+  set setAxis(int data) {
+    _crossAxisCount = data;
+    notifyListeners();
+  }
+
+  set setexpandedHeight(double data) {
+    _expandedHeight = data;
+    notifyListeners();
+  }
+
+  set setImageHeight(double data) {
+    _imageHeight = data;
+    notifyListeners();
+  }
+
+  List<Product> get products => _productList;
+}
